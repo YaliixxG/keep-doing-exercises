4f4c7df4fdd82df3d0cdd486dc7a54ee8744cd4c
YaliixxG <529008421@qq.com>
2018-09-17 11:42:01 +0800

    add:数组加一

---
+++ 66.plus-one.js
@@ -0,0 +1,48 @@
+/*
+ * [66] Plus One
+ *
+ * https://leetcode-cn.com/problems/plus-one/description/
+ *
+ * algorithms
+ * Easy (35.07%)
+ * Total Accepted:    19.1K
+ * Total Submissions: 54.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。
+ * 
+ * 最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。
+ * 
+ * 你可以假设除了整数 0 之外，这个整数不会以零开头。
+ * 
+ * 示例 1:
+ * 
+ * 输入: [1,2,3]
+ * 输出: [1,2,4]
+ * 解释: 输入数组表示数字 123。
+ * 
+ * 
+ * 示例 2:
+ * 
+ * 输入: [4,3,2,1]
+ * 输出: [4,3,2,2]
+ * 解释: 输入数组表示数字 4321。
+ * 
+ * 
+ */
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+var plusOne = function(digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] == 9) {
+      digits[i] = 0
+    } else {
+      digits[i]++
+      break
+    }
+  }
+  if (digits[0] == 0) digits.unshift(1)
+  return digits
+}
--- README.md
+++ README.md
@@ -25,3 +25,5 @@
 11. 给定一个正整数 n（1 ≤ n ≤ 30），输出报数序列的第 n 项。[解答](https://github.com/YaliixxG/keep-doing-exercises/blob/master/38.count-and-say.js)
 
 12. 给定一个整数数组 nums ，找到一个具有最大和的连续子数组（子数组最少包含一个元素），返回其最大和。[解答](https://github.com/YaliixxG/keep-doing-exercises/blob/master/53.maximum-subarray.js)
+
+13. 给定一个由整数组成的非空数组所表示的非负整数，在该数的基础上加一。最高位数字存放在数组的首位， 数组中每个元素只存储一个数字。[解答](https://github.com/YaliixxG/keep-doing-exercises/blob/master/66.plus-one.js)
